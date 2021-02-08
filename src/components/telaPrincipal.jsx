910a1a8b0e05f805515f3f9545971bcab1c95579
Marcos Souza <marcos_okto@yahoo.com.br>
2021-02-08 13:59:45 -0300

    instalado o material-ui via comando, atraves desse pacote, foi criado um botao TEXTFIELD na telaPrincipal.jsx

--- src/components/telaPrincipal.jsx
+++ src/components/telaPrincipal.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import PuxarCep from './puxarCep';
+import {Box} from '@material-ui/core';
+import TextField  from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+      root: {
+        '& > *': {
+          margin: theme.spacing(1),
+          width: '25ch',
+        },
+      },
+    }));
 
 const TelaPrincipal = () => {
       return(
-            
+            <Box>
+                  <TextField id="outlined-basic" label="Digite o CEP" variant="outlined" />
+            </Box>
       );
 }
 
